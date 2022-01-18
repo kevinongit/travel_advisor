ffdc14492290c97d97feb24e182fd5bdcd651ffc
kevin <nait90@gmail.com>
2022-01-18 16:50:47 +0900

    type added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {CssBaseline, Grid} from '@material-ui/core'
+
+import {getPlacesData} from './api'
 
 import Header from './components/Header/Header'
 import Map from './components/Map/Map'
 import List from './components/List/List'
 
 const App = () => {
+  const [places, setPlaces] = useState([])
+  const [childClicked, setChildClicked] = useState(null)
+  const [coordinates, setCoordinates] = useState({})
+  const [bounds, setBounds] = useState({})
+
+  const [type, setType] = useState('restaurants')
+  const [rating, setRating] = useState('')
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({coords: {latitude, longitude}}) => {
+        setCoordinates({lat: latitude, lng: longitude})
+      },
+    )
+  }, [])
+  useEffect(() => {
+    setIsLoading(true)
+    getPlacesData(type, bounds.sw, bounds.ne).then(data => {
+      // console.log(data)
+      setPlaces(data)
+      setIsLoading(false)
+    })
+  }, [type, bounds, coordinates])
   return (
     <>
       <CssBaseline />
       <Header />
       <Grid container spacing={3} style={{width: '100%'}}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List
+            places={places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            places={places}
+            setChildClicked={setChildClicked}
+          />
         </Grid>
       </Grid>
     </>
